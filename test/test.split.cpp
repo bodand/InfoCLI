8f41cc46101a4b421944bb97b2fb98b310819e3a
bodand <bodand@pm.me>
2020-05-16 14:36:55 +0200

    compile-time string splitter

--- test/test.split.cpp
+++ test/test.split.cpp
@@ -1,22 +1,22 @@
 //// BSD 3-Clause License
-//
+// 
 // Copyright (c) 2020, bodand
 // All rights reserved.
-//
+// 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-//
+// 
 // 1. Redistributions of source code must retain the above copyright notice, this
 //    list of conditions and the following disclaimer.
-//
+// 
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-//
+// 
 // 3. Neither the name of the copyright holder nor the names of its
 //    contributors may be used to endorse or promote products derived from
 //    this software without specific prior written permission.
-//
+// 
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -29,53 +29,52 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //
-// Created by bodand on 2020-05-15.
+// Created by bodand on 2020-05-16.
 //
 
-#pragma once
+#include <catch2/catch.hpp>
 
-template<class>
-struct type_t {
-};
+#include <boost/hana/string.hpp>
+namespace hana = boost::hana;
+using namespace hana::literals;
 
-template<class T>
-constexpr const static auto type_c = type_t<T>{};
+// project
+#include "type_equality.hpp"
 
-template<class T, class U>
-bool operator==(type_t<T>, type_t<U>) {
-    return false;
-}
+// test'd
+#include <info/cli/split.hpp>
+using namespace info::cli::impl;
 
-template<class T>
-bool operator==(type_t<T>, type_t<T>) {
-    return true;
-}
+TEST_CASE("split test cases", "[split][impl][meta]") {
+    SECTION("split returns one element tuple for non-splittable string") {
+        auto str = BOOST_HANA_STRING("text");
+        auto ret = split(str);
 
-template <typename T>
-constexpr auto
-type_name()
-{
-    std::string_view name, prefix, suffix;
-#ifdef __clang__
-    name = __PRETTY_FUNCTION__;
-    prefix = "auto type_name() [T = ";
-    suffix = "]";
-#elif defined(__GNUC__)
-    name = __PRETTY_FUNCTION__;
-    prefix = "constexpr auto type_name() [with T = ";
-    suffix = "]";
-#elif defined(_MSC_VER)
-    name = __FUNCSIG__;
-    prefix = "class auto __cdecl type_name<";
-    suffix = ">(void)";
-#endif
-    name.remove_prefix(prefix.size());
-    name.remove_suffix(suffix.size());
-    return name;
-}
+        CHECK(type_c<std::remove_reference_t<decltype(ret[0_c])>> == type_c<decltype(str)>);
+    }
 
-template<class T>
-std::ostream& operator<<(std::ostream& os, const type_t<T>&) {
-    os << type_name<T>();
-    return os;
+    SECTION("split splits string with one | in it") {
+        auto str = BOOST_HANA_STRING("te|xt");
+        auto ret = split(str);
+        auto exp = hana::make_tuple(
+               BOOST_HANA_STRING("te"),
+               BOOST_HANA_STRING("xt")
+        );
+
+        CHECK(type_c<decltype(ret)> == type_c<decltype(exp)>);
+    }
+
+    SECTION("split splits string with multiple | in it") {
+        auto str = BOOST_HANA_STRING("te|xt|te|xt|t");
+        auto ret = split(str);
+        auto exp = hana::make_tuple(
+               BOOST_HANA_STRING("te"),
+               BOOST_HANA_STRING("xt"),
+               BOOST_HANA_STRING("te"),
+               BOOST_HANA_STRING("xt"),
+               BOOST_HANA_STRING("t")
+        );
+
+        CHECK(type_c<decltype(ret)> == type_c<decltype(exp)>);
+    }
 }
