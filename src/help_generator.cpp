7e82d43efe613baf22fd853ba060be212ec60f9b
bodand <bodand@pm.me>
2020-05-21 13:29:35 +0200

    tweak auto-help message

--- src/help_generator.cpp
+++ src/help_generator.cpp
@@ -1,22 +1,22 @@
 //// BSD 3-Clause License
-// 
+//
 // Copyright (c) 2020, bodand
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright notice, this
 //    list of conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 // 3. Neither the name of the copyright holder nor the names of its
 //    contributors may be used to endorse or promote products derived from
 //    this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -45,16 +45,16 @@ void info::cli::impl::help_generator::help_str::add_opt(std::string_view str) no
 
 std::string
 info::cli::impl::help_generator::help_str::print() const noexcept {
-    if (__builtin_expect(_opts.size() > 16, 0)) {
-        return fmt::format("  {}\n", _opts);
+    if (__builtin_expect(_opts.size() > 24, 0)) {
+        return fmt::format("  {}\n                        ", _opts);
     } else {
-        return fmt::format("  {:<14}", _opts);
+        return fmt::format("  {:<22}", _opts);
     }
 }
 
 void
 info::cli::impl::help_generator::print() const noexcept {
-    fmt::print("Usage: {} [options]\n", _name);
+    fmt::print("Usage: {} [options]\n\n", _name);
     fmt::print("Possible options:\n");
     for (auto&&[help, opts] : _helps) {
         fmt::print("  {} {}\n", opts.print(), help);
